599670e4a03159e078cbbd059a5e136755709443
jak2002 <jacktonmulongo8@gmail.com>
2026-08-18 21:02:35 +0300

    Use this connection from multiple threads

--- chatbot.py
+++ chatbot.py
@@ -334,7 +334,7 @@ class Database:
 
     def __init__(self, db_path: str):
         self.db_path = db_path
-        self._conn = sqlite3.connect(self.db_path)
+        self._conn = sqlite3.connect(self.db_path, check_same_thread=False)
         self._conn.row_factory = sqlite3.Row
         self._conn.execute("PRAGMA foreign_keys = ON")
         self._init_schema()
